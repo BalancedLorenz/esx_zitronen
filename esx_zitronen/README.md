c5d0e09a6bed569c7fd1640d445b257b14bb3179
BalancedLorenz <70029771+BalancedLorenz@users.noreply.github.com>
2020-08-21 19:41:57 +0200

    Add files via upload

---
+++ esx_zitronen/README.md
@@ -0,0 +1,31 @@
+# esx_zitronen
+
+Update 0.5
+
+E Drücken zum Zitronen Farmen
+
+Items für die Datenbank:
+-Zitronen   
+-Zitronensaft
+
+# Installation
+1. Script installieren
+3. In den Resources Ordner einfügen
+4. Die Items in die Datenbank importieren
+5. esx_zitronen in die Servercfg einfügen
+
+
+
+
+# Created by
+- Jordann](https://github.com/jordann124)
+
+# Edited by
+- BalancedLorenz (https://github.com/BalancedLorenz)
+- New Server and Client Events
+
+# Discord
+https://discord.gg/3gJrbxH
+
+
+# Legal
---
+++ esx_zitronen/locales/en.lua
@@ -0,0 +1,17 @@
+Locales['en'] = {
+	['carry_for_leaves'] = 'Drücke ~INPUT_CONTEXT~ um Zitronen zu farmen',
+	['take_orange_juice'] = 'Drücke ~INPUT_CONTEXT~ um deine Zitronen zu pressen',
+	['sell_orage_juice'] = 'Drücke ~INPUT_CONTEXT~ zum deine fischen Zitronensaft zu verkaufen',
+	['bag_full'] = 'Dein Rucksack ist voll.',
+	['take_orage'] = '~y~Du sammelst Zitronen~s~...',
+	['you_do_not_have_enough_oranges'] = 'Du hast nicht genügend Zitronen.',
+	['you_do_not_have_any_more_oranges'] = 'Du hast nicht mehr Zitronensaft',
+	['transform_juice_orage'] = '~y~Du Presst deine Zitronen~s~...',
+	['you_do_not_have_juice_orage'] = 'Du hast nicht mehr~r~Zitronensaft~s~',
+	['sell_juice'] = 'Verkaufe ~y~x1~s~ ~y~Zitronensaft~s~',
+	['sell_juice_orange'] = '~g~Verkauf gestartet~...',
+	-- Blips 
+	['orange_picking'] = 'Zitronenfarm',
+	['turns_from_juice'] = 'Zitronenpressung',
+	['sell_juice_orage_blip'] = 'Zitronensaftverkauf',
+}
